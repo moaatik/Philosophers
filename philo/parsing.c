3e5577598b2312d6093a2ff72263aa2deda7f61a
Mohamed Aatik <moaatik@e2r2p15.1337.ma>
2025-04-11 16:23:55 +0100

    norms

--- philo/parsing.c
+++ philo/parsing.c
@@ -6,11 +6,12 @@
 /*   By: moaatik <moaatik@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/03 10:25:21 by moaatik           #+#    #+#             */
-/*   Updated: 2025/04/11 16:14:30 by moaatik          ###   ########.fr       */
+/*   Updated: 2025/04/11 16:23:43 by moaatik          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philo.h"
+
 int	input(int ac, char **av, t_table *table)
 {
 	int	error;
@@ -24,7 +25,7 @@ time_to_die eat_time sleep_time max_meals(optional)\n", 101), 1);
 	{
 		error = 0;
 		if (ft_atoi(av[i++], &error) <= 0 || error)
-			return (write(2, "Error\nYou have entered an invalid argument\n", 43), 1);
+			return (write(2, "Error\ninvalid argument\n", 43), 1);
 	}
 	table->philos_number = ft_atoi(av[1], &error);
 	table->time_to_die = ft_atoi(av[2], &error);
